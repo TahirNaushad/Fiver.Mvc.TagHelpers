5829370eceac57ee6865e0652cf3edf476ff46df
Tahir Naushasd <t.naushad25@gmail.com>
2017-06-28 10:18:15 +0100

    setup

---
+++ Fiver.Mvc.TagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Fiver.Mvc.TagHelpers.Models.Home;
+using Newtonsoft.Json;
+
+namespace Fiver.Mvc.TagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new EmployeeViewModel
+            {
+                Id = 5
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(EmployeeViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var json = JsonConvert.SerializeObject(model);
+            return Content(json);
+        }
+
+        public IActionResult Echo(int id)
+        {
+            return Content(id.ToString());
+        }
+
+        public IActionResult NamedRoute(int id)
+        {
+            return Content(id.ToString());
+        }
+    }
+}
